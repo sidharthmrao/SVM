13617816d76cb63ad796447eaba0ca3572387fd2
Sidharth Rao <sidharthmrao@gmail.com>
2022-02-25 02:36:52 -0500

    Deleted useless Main

--- main.py
+++
@@ -1,7 +0,0 @@
-# https://towardsdatascience.com/implementing-svm-from-scratch-784e4ad0bc6a
-# https://towardsdatascience.com/gradient-descent-from-scratch-e8b75fa986cc
-
-
-#import numpy as np
-#import pandas as pd
-
